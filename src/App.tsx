ebc164277e5fa7cff150ff194df68f05ec20bea4
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-04-19 17:15:43 +0200

    Fixed environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,86 @@
 import React, { useState, useContext } from "react";
 import WeatherContext from "./weatherContext";
 
-/* TODO: 
- - Move api to other component
- - Split up into components
- - Add sass
- - Add more background images
- - Create helper functions
- - Sockets?
- - Accessability
- - Display error message
-*/
-
 const App = () => {
-	// TODO: Set types
-	const [query, setQuery] = useState("");
-	const { weather, search } = useContext(WeatherContext);
+  // TODO: Set types
+  const [query, setQuery] = useState("");
+  const { weather, search } = useContext(WeatherContext);
 
-	const handleKeyPress: (e: React.KeyboardEvent) => void = (e) => {
-		if (e.key === "Enter") search(query);
-	};
+  const handleKeyPress: (e: React.KeyboardEvent) => void = (e) => {
+    if (e.key === "Enter") search(query);
+  };
 
-	const dateBuilder: (d: Date) => string = (d) => {
-		const mapMonthToNiceName = [
-			"January",
-			"February",
-			"March",
-			"April",
-			"May",
-			"June",
-			"July",
-			"August",
-			"September",
-			"October",
-			"November",
-			"December"
-		];
-		const mapDayToNiceName = [
-			"Monday",
-			"Tuesday",
-			"Wednesday",
-			"Thursday",
-			"Friday",
-			"Saturday",
-			"Sunday"
-		];
+  const dateBuilder: (d: Date) => string = (d) => {
+    const mapMonthToNiceName = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const mapDayToNiceName = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
 
-		const month = mapMonthToNiceName[d.getMonth()];
-		const date = d.getDate();
-		const day = mapDayToNiceName[d.getDay()];
-		const year = d.getFullYear();
+    const month = mapMonthToNiceName[d.getMonth()];
+    const date = d.getDate();
+    const day = mapDayToNiceName[d.getDay()];
+    const year = d.getFullYear();
 
-		return `${day} ${date} ${month} ${year}`;
-	};
+    return `${day} ${date} ${month} ${year}`;
+  };
 
-	return (
-		<div
-			className={`app ${
-				weather.temperature && weather.temperature > 16
-					? "warm"
-					: "cold"
-			}`}
-		>
-			<main>
-				<div className="search-box">
-					<input
-						type="text"
-						className="search-bar"
-						placeholder="Search..."
-						onChange={(e) => setQuery(e.target.value)}
-						value={query}
-						onKeyPress={handleKeyPress}
-					/>
-				</div>
-				{weather.valid ? (
-					<>
-						<div className="location-box">
-							<div className="location">
-								{weather.name}, {weather.country}
-							</div>
-							<div className="date">
-								{dateBuilder(new Date())}
-							</div>
-						</div>
-						<div className="weather-box">
-							<div className="temp">
-								{Math.round(
-									weather.temperature
-										? weather.temperature
-										: 0
-								)}{" "}
-								°C
-							</div>
-							<div className="weather">{weather.weather}</div>
-						</div>
-					</>
-				) : (
-					<></>
-				)}
-			</main>
-		</div>
-	);
+  return (
+    <div
+      className={`app ${
+        weather.temperature && weather.temperature > 16 ? "warm" : "cold"
+      }`}
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={handleKeyPress}
+          />
+        </div>
+        {weather.valid ? (
+          <>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.temperature ? weather.temperature : 0)} °C
+              </div>
+              <div className="weather">{weather.weather}</div>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </main>
+    </div>
+  );
 };
 
 export default App;
--- src/weatherContext.tsx
+++ src/weatherContext.tsx
@@ -1,78 +1,88 @@
 import React, { createContext, useState } from "react";
 
 interface Weather {
-	temperature: number;
-	weather: string;
-	name: string;
-	country: string;
-	valid: boolean;
+  temperature: number;
+  weather: string;
+  name: string;
+  country: string;
+  valid: boolean;
 }
+
 const initialWeather = {
-	temperature: -1,
-	weather: "",
-	name: "",
-	country: "",
-	valid: false
+  temperature: -1,
+  weather: "",
+  name: "",
+  country: "",
+  valid: false,
 };
 
 type Search = (query: string) => void;
 const initialSearch = () => {
-	throw new Error("Search not declared");
+  throw new Error("Search not declared");
 };
 
 type ContextValue = {
-	weather: Weather;
-	search: Search;
-	error: string | null;
+  weather: Weather;
+  search: Search;
+  error: string | null;
 };
 
 const WeatherContext = createContext<ContextValue>({
-	weather: initialWeather,
-	search: initialSearch,
-	error: null
+  weather: initialWeather,
+  search: initialSearch,
+  error: null,
 });
 
 const api = {
-	key: process.env.API_KEY,
-	baseUrl: "https://api.openweathermap.org/data/2.5"
+  key: process.env.REACT_APP_API_KEY,
+  baseUrl: "https://api.openweathermap.org/data/2.5",
 };
 
 interface Props {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
+const mapErrorCodeToMessage: (code: number) => string = (code) => {
+  switch (code) {
+    case 401:
+      return "The API key is invalid. Please supply another key";
+    case 404:
+      return "Location not found. Check your spelling and try again!";
+    default:
+      return "Woops, something went wrong!";
+  }
+};
+
 const WeatherProvider: React.FC<Props> = ({ children }) => {
-	const [weatherState, setWeather] = useState<Weather>(initialWeather);
-	const [error, setError] = useState<string | null>(null);
+  const [weatherState, setWeather] = useState<Weather>(initialWeather);
+  const [error, setError] = useState<string | null>(null);
 
-	const search: (query: string) => void = (query) => {
-		fetch(`${api.baseUrl}/weather?q=${query}&units=metric&appid=${api.key}`)
-			.then((res) => res.json())
-			.then((res) => {
-				if (res.cod === "404") {
-					setWeather(initialWeather);
-					// TODO: implement error handling
-					setError("Location not found");
-				} else {
-					setError(null);
-					setWeather({
-						temperature: res.main.temp,
-						weather: res.weather[0].main,
-						name: res.name,
-						country: res.sys.country,
-						valid: true
-					});
-				}
-			});
-	};
+  const search: (query: string) => void = (query) => {
+    fetch(`${api.baseUrl}/weather?q=${query}&units=metric&appid=${api.key}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (+res.cod >= 400) {
+          setWeather(initialWeather);
+          setError(mapErrorCodeToMessage(+res.cod));
+        } else {
+          setError(null);
+          setWeather({
+            temperature: res.main.temp,
+            weather: res.weather[0].main,
+            name: res.name,
+            country: res.sys.country,
+            valid: true,
+          });
+        }
+      });
+  };
 
-	return (
-		<WeatherContext.Provider
-			value={{ weather: weatherState, search, error }}
-		>
-			{children}
-		</WeatherContext.Provider>
-	);
+  return (
+    <WeatherContext.Provider value={{ weather: weatherState, search, error }}>
+      {children}
+    </WeatherContext.Provider>
+  );
 };
 
 export { WeatherProvider };
